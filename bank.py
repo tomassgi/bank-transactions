18baacdf8bfb7ee62def41c588b259af9f7920e1
Ilya Karpovs <karpov.ilja@gmail.com>
2024-01-17 10:43:04 +0200

    Initial version

---
+++ bank.py
@@ -0,0 +1,41 @@
+# 
+# Darbības ar masīviem - https://www.w3schools.com/python/python_arrays.asp
+# 
+
+transactions = []
+
+def deposit(transactions, amount):
+    pass
+
+def withdraw(transactions, amount):
+    pass
+
+def check_balance(transactions):
+    pass
+
+def list():
+    pass
+
+while True:
+    print("\nBanking Options:")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Check Balance")
+    print("4. List transactions")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        pass
+    elif choice == '5':
+        print("Exiting the banking system. Thank you!")
+        break
+    else:
+        print("Invalid choice. Please enter a number between 1 and 5.")
